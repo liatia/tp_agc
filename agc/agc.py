e4128c20c492c048f13e807287dff802bc8942c6
cplessis <clement.plessis85@gmail.com>
2020-10-22 14:52:24 +0200

    ajout des premieres fonctions

--- agc/agc.py
+++ agc/agc.py
@@ -23,13 +23,13 @@ from collections import Counter
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
 import nwalign3 as nw
 
-__author__ = "Your Name"
+__author__ = "Lara Herrmann"
 __copyright__ = "Universite Paris Diderot"
-__credits__ = ["Your Name"]
+__credits__ = ["Lara Herrmann"]
 __license__ = "GPL"
 __version__ = "1.0.0"
-__maintainer__ = "Your Name"
-__email__ = "your@email.fr"
+__maintainer__ = "Lara Herrmann"
+__email__ = "lara.herrma@gmail.com"
 __status__ = "Developpement"
 
 
@@ -55,7 +55,8 @@ def get_arguments():
     parser = argparse.ArgumentParser(description=__doc__, usage=
                                      "{0} -h"
                                      .format(sys.argv[0]))
-    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, required=True, 
+    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, 
+                        required=True, 
                         help="Amplicon is a compressed fasta file (.fasta.gz)")
     parser.add_argument('-s', '-minseqlen', dest='minseqlen', type=int, default = 400,
                         help="Minimum sequence length for dereplication")
@@ -69,6 +70,104 @@ def get_arguments():
                         default="OTU.fasta", help="Output file")
     return parser.parse_args()
 
+
+def read_fasta(amplicon_file, minseqlen):
+    """ Lecture du fichier fastq en entree
+        Retourne : générateur de séquences de longueur l >= minseqlen: yield sequence
+    """
+    if amplicon_file.endswith(".gz"):
+        with gzip.open(amplicon_file, "rb") as filin:
+            sequence = b''
+            for line in filin:
+                if line.startswith(b">"):
+                    if (len(sequence) >= minseqlen):
+                        yield sequence.decode('ascii')
+                    sequence = b''
+                else:
+                    sequence += line.strip()
+        yield sequence.decode('ascii')
+    else:
+        with open(amplicon_file, "r") as filin:
+            sequence = ''
+            for line in filin:
+                if line.startswith(">"):
+                    if (len(sequence) >= minseqlen):
+                        yield sequence
+                    sequence = ''
+                else:
+                    sequence += line.strip()
+        yield sequence
+
+
+def dereplication_fulllength(amplicon_file, minseqlen, mincount):
+    occu_dict = {}
+    del_list = []
+    for sequence in list(read_fasta(amplicon_file, minseqlen)):
+        if not sequence in occu_dict:
+            occu_dict[sequence] = 0
+        occu_dict[sequence] += 1
+    for sequence in occu_dict:
+        if occu_dict[sequence] < mincount:
+            del_list.append(sequence)
+    for sequence in sorted(occu_dict, key=occu_dict.get, reverse=True):
+        if sequence in del_list:
+            del occu_dict[sequence]
+        else:
+            yield(sequence, occu_dict[sequence])
+
+
+def get_chunks(sequence, chunk_size):
+    print("nouvel appel")
+    chunk_list = []
+    start = 0
+    end = chunk_size
+    while end < len(sequence):
+        chunk_list.append(sequence[start:end])
+        print(len(sequence[start:end]))
+        start += chunk_size
+        end += chunk_size
+    if len(chunk_list) < 4 :
+        raise(ValueError("Less than 4 chunks"))
+    return chunk_list
+
+
+def cut_kmer(sequence, kmer_size):
+    """ Coupe les sequences en kmers
+        Retourne : tous les kmers trouves dans les sequences
+    """
+    for i in range(len(sequence) - kmer_size + 1):
+        yield sequence[i:i + kmer_size]
+
+
+def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+    for kmer in list(cut_kmer(sequence, kmer_size)):
+        if kmer in kmer_dict:
+            
+
+
+def search_mates(kmer_dict, sequence, kmer_size):
+    pass
+
+
+def get_identity(alignment_list):
+    pass
+
+
+def detect_chimera(perc_identity_matrix):
+    pass
+
+
+def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
+    pass
+
+
+def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
+    pass
+
+
+def write_OTU(OTU_list, output_file):
+    pass
+
 #==============================================================
 # Main program
 #==============================================================
@@ -79,6 +178,8 @@ def main():
     # Get arguments
     args = get_arguments()
 
+    # De-deuplication en sequence complete 
+    dereplication_fulllength(args.amplicon_file, args.minseqlen, args.mincount)
 
 if __name__ == '__main__':
     main()
