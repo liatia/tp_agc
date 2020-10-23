e3367ad03c5d0043f1a1a692deb0485f14a1af8c
cplessis <clement.plessis85@gmail.com>
2020-10-23 18:59:49 +0200

    version finale

--- agc/agc.py
+++ agc/agc.py
@@ -18,10 +18,10 @@ import sys
 import os
 import gzip
 import statistics
+import hashlib
 from collections import Counter
 # https://github.com/briney/nwalign3
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
-import statistics
 import nwalign3 as nw
 
 __author__ = "Lara Herrmann"
@@ -56,8 +56,8 @@ def get_arguments():
     parser = argparse.ArgumentParser(description=__doc__, usage=
                                      "{0} -h"
                                      .format(sys.argv[0]))
-    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, 
-                        required=True, 
+    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile,
+                        required=True,
                         help="Amplicon is a compressed fasta file (.fasta.gz)")
     parser.add_argument('-s', '-minseqlen', dest='minseqlen', type=int, default = 400,
                         help="Minimum sequence length for dereplication")
@@ -74,14 +74,15 @@ def get_arguments():
 
 def read_fasta(amplicon_file, minseqlen):
     """ Lecture du fichier fastq en entree
-        Retourne : générateur de séquences de longueur l >= minseqlen: yield sequence
+        Retourne : générateur de séquences de longueur l >= minseqlen:
+        yield sequence
     """
     if amplicon_file.endswith(".gz"):
         with gzip.open(amplicon_file, "rb") as filin:
             sequence = b''
             for line in filin:
                 if line.startswith(b">"):
-                    if (len(sequence) >= minseqlen):
+                    if len(sequence) >= minseqlen:
                         yield sequence.decode('ascii')
                     sequence = b''
                 else:
@@ -92,7 +93,7 @@ def read_fasta(amplicon_file, minseqlen):
             sequence = ''
             for line in filin:
                 if line.startswith(">"):
-                    if (len(sequence) >= minseqlen):
+                    if len(sequence) >= minseqlen:
                         yield sequence
                     sequence = ''
                 else:
@@ -101,6 +102,15 @@ def read_fasta(amplicon_file, minseqlen):
 
 
 def dereplication_fulllength(amplicon_file, minseqlen, mincount):
+    """
+    Prend trois arguments correspondant au fichier fasta,  la longueur
+    minimale des séquences et leur comptage minimum. Elle fait appel au
+    générateur fourni par read_fasta et retourne un générateur des séquences
+    uniques ayant une occurrence O>=mincount ainsi que leur occurrence.
+    Les séquences seront retournées par ordre décroissant d’occurrence:
+    yield [sequence, count]
+
+    """
     occu_dict = {}
     del_list = []
     for sequence in list(read_fasta(amplicon_file, minseqlen)):
@@ -110,14 +120,20 @@ def dereplication_fulllength(amplicon_file, minseqlen, mincount):
     for sequence in occu_dict:
         if occu_dict[sequence] < mincount:
             del_list.append(sequence)
+    for sequence in del_list:
+        print("avant: ",len(occu_dict))
+        del occu_dict[sequence]
+        print("après: ",len(occu_dict))
     for sequence in sorted(occu_dict, key=occu_dict.get, reverse=True):
-        if sequence in del_list:
-            del occu_dict[sequence]
-        else:
-            yield(sequence, occu_dict[sequence])
+        yield(sequence, occu_dict[sequence])
 
 
 def get_chunks(sequence, chunk_size):
+    """ Prend une séquence et un longueur de segment l:
+    chunk_size et retourne une liste de sous-séquences de
+    taille l non chevauchantes. A minima 4 segments doivent
+    être obtenus par séquence.
+    """
     chunk_list = []
     start = 0
     end = chunk_size
@@ -126,7 +142,7 @@ def get_chunks(sequence, chunk_size):
         start += chunk_size
         end += chunk_size
     if len(chunk_list) < 4 :
-        raise(ValueError("Less than 4 chunks"))
+        raise ValueError("Less than 4 chunks")
     return chunk_list
 
 
@@ -139,6 +155,9 @@ def cut_kmer(sequence, kmer_size):
 
 
 def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+    """ prend un dictionnaire ayant pour clé un index de kmer et
+    pour valeur une liste d’identifiant des séquences dont ils proviennent
+    """
     for kmer in list(cut_kmer(sequence, kmer_size)):
         if kmer in kmer_dict:
             kmer_dict[kmer].append(id_seq)
@@ -148,13 +167,23 @@ def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
 
 
 def search_mates(kmer_dict, sequence, kmer_size):
-    return [i[0] for i in Counter([ids for kmer in cut_kmer(sequence, kmer_size) if kmer in kmer_dict for ids in kmer_dict[kmer]]).most_common(8)]
+    """
+    Prend un dictionnaire ayant pour clé un index de kmer et pour valeur
+    une liste d’identifiant des séquences dont ils proviennent, une séquence
+    et une longueur de kmer: kmer_size.
+
+    """
+    return [i[0] for i in Counter([ids for kmer in cut_kmer(sequence, kmer_size) \
+        if kmer in kmer_dict for ids in kmer_dict[kmer]]).most_common(8)]
 
 
 def get_identity(alignment_list):
+    """prend un alignement (sous forme de liste) et calcule le pourcentage
+    d’identité entre les deux séquences selon la formule.
+    """
     indentic_nucl = 0
-    for nt in zip(alignment_list[0], alignment_list[1]):
-        if nt[0] == nt[1]:
+    for nucl in zip(alignment_list[0], alignment_list[1]):
+        if nucl[0] == nucl[1]:
             indentic_nucl += 1
     alignemnt_length = len(alignment_list[0])
     return indentic_nucl/alignemnt_length * 100
@@ -162,9 +191,9 @@ def get_identity(alignment_list):
 
 def detect_chimera(perc_identity_matrix):
     """
-    Si l’écart type moyen des pourcentages est supérieur à 5 et 
-    que 2 segments minimum de notre séquence montrent une similarité 
-    différente à un des deux parents, nous identifierons cette 
+    Si l’écart type moyen des pourcentages est supérieur à 5 et
+    que 2 segments minimum de notre séquence montrent une similarité
+    différente à un des deux parents, nous identifierons cette
     séquence comme chimérique.
     """
     std_list = []
@@ -189,16 +218,75 @@ def detect_chimera(perc_identity_matrix):
     return False
 
 
+def common(lst1, lst2):
+    """ Retourne les elements communs entre deux listes.
+    """
+    return list(set(lst1) & set(lst2))
+
+
 def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+    """Fait appel au générateur fourni par dereplication_fulllength et
+    retourne un générateur des séquences non chimérique au format:
+    yield [sequence, count]
+    """
+    kmer_dict = {}
+    perc_identity_matrix = []
+    chunk_match = []
+    seq_list = []
+    chim_id = 0
+    for i, occurence_list in enumerate(list(dereplication_fulllength(amplicon_file, minseqlen,
+                                                                     mincount))):
+        chim = True
+        chunk_list = get_chunks(occurence_list[0], chunk_size)
+        for chunk in chunk_list:
+            chunk_match.append(search_mates(kmer_dict, chunk, kmer_size))
+        com_seq = common(chunk_match[0], chunk_match[1])
+        for j in range(2, len(chunk_match)):
+            com_seq = common(com_seq, chunk_match[j])
+        if len(com_seq) > 1:
+            for k in range(len(chunk_list)):
+                perc_identity_matrix.append([][k])
+            for seq in com_seq[0:2]:
+                seq_chunk_list = get_chunks(seq_list[seq], chunk_size)
+                for l, chunk in enumerate(chunk_list):
+                    perc_identity_matrix[l].append(get_identity(
+                        nw.global_align(chunk, seq_chunk_list[l],
+                            gap_open = -1, gap_extend = 1, matrix = "MATCH")))
+            chim = detect_chimera(perc_identity_matrix)
+        else:
+            chim = False
+        if not chim:
+            kmer_dict = get_unique_kmer(kmer_dict, occurence_list[0], chim_id, kmer_size)
+            seq_list.append(occurence_list[0])
+            chim_id += 1
+            yield occurence_list
 
 
 def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+    """ Fait appel à chimera removal et retourne un liste d’OTU,
+    cette liste indiquera pour chaque séquence son occurrence (count).
+    """
+    OTU_list = []
+    occurence_list =  list(chimera_removal(amplicon_file, minseqlen,
+                                           mincount, chunk_size, kmer_size))
+    for seq, occurrence in occurence_list:
+        OTU_list.append((seq, occurrence))
+    return OTU_list
+
+
+def fill(text, width=80):
+    """Split text with a line return to respect fasta format"""
+    return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
 
 
 def write_OTU(OTU_list, output_file):
-    pass
+    """ Ecrit un fichier de sortie contenant les OTU
+    """
+    with open(output_file, "w") as filout:
+        for i, otu in enumerate(OTU_list):
+            filout.write(">OTU_" + str(i + 1) + " occurrence:" + str(otu[1]) + "\n")
+            filout.write(fill(str(otu[0]))+"\n")
+
 
 #==============================================================
 # Main program
@@ -210,24 +298,10 @@ def main():
     # Get arguments
     args = get_arguments()
 
+    OTU_list = abundance_greedy_clustering(args.amplicon_file, args.minseqlen, args.mincount,
+                                           args.chunk_size, args.kmer_size)
+    write_OTU(OTU_list, args.output_file)
+
 
 if __name__ == '__main__':
-    # main()
-    assert(not detect_chimera([[73.58, 73.79], [72.97, 77.06], [77.36, 80.58], [78.43, 78.43]]))
-    assert(not detect_chimera([[62.6, 94.17], [62.6, 94.17], [62.6, 94.17], [62.6, 94.17]]))
-    assert(detect_chimera([[98.0, 60.0], [100.0, 65.0], [100.0, 63.0], [64.0, 100.0]]))
-    S000387216 = "GGAGGCTCGTACCGCTGTCTTGTTAAGGACTGGTTTTTTACTGTCTATACAGACTCTTCATACTACTGGATATCCTGATATGCGTTCGGATCGATTGTTGCCGTACGCTGTGTCGATTAAAGGTAATCATAAGGGCTTTCGACTTACGACTC"
-    chimera_AJ007403 = "AAGACGCTTGGGTTTCACTCCTGCGCTTCGGCCGGGCCCGGCACTCGCCACAGTCTCGAGCGTCGTCTTGATGTTCACATTGCGTTCGGATCGATTGTTGCCGTACGCCTGTGTCATTAAAGGTAATCATAAGGGCTTTCGACTTACGACTC"
-    S000001688 = "AAGACGCTTGGGTTTCACTCCTGCGCTTCGGCCGGGCCCGGCACTCGCCACAGTCTCGAGCGTCGTCTTGATGTTCACATGTAACGATCGCTTCCAACCCATCCGGTGCTGTGTCGCCGGGCACGGCTTGGGAATTAACTATTCCCAAGTCT"
-    chunk_chim = get_chunks(chimera_AJ007403, 37)
-    chunk_seq_list = [get_chunks(S000387216, 37)]
-    chunk_seq_list += [get_chunks(S000001688, 37)]
-    perc_identity_matrix = [[] for c in range(len(chunk_chim))]
-    for i in range(len(chunk_seq_list)):
-        for l,chunk in enumerate(chunk_chim):
-            perc_identity_matrix[l].append(get_identity(
-                        nw.global_align(chunk, chunk_seq_list[i][l], 
-                            gap_open=-1, gap_extend=-1, matrix=os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                '../agc')) + "/MATCH")))
-    # [[48.89, 100.0], [51.16, 100.0], [84.62, 58.54], [94.74, 45.65]]
-    assert(detect_chimera(perc_identity_matrix))
+    main()
